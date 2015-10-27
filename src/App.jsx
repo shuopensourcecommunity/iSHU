fb23d42d0f34f58c244aae8a91c654d5f9fe5cb2
ywtesi <jyuwei0123@gmail.com>
2015-10-28 02:14:48 +0800

    update home button in AppBar

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@
 let React = require('react');
 let {render} = require('react-dom');
 let AppBar = require('./AppBar.jsx');
-let {CardMedia, CardTitle} = require('material-ui');
 var {Link, RouteHandler} = require('react-router');
 
 let HomeTable = React.createClass({
--- src/AppBar.jsx
+++ src/AppBar.jsx
@@ -1,15 +1,27 @@
 'use strict'
-
-var React = require('react');
-var {render} = require('react-dom');
-var Mui = require('material-ui');
-var AppBarComponent = Mui.AppBar;
 require("../style/css/main.css");
+let React = require('react');
+let {render} = require('react-dom');
+let mui = require('material-ui');
+let AppBarComponent = mui.AppBar;
+let IconButton = mui.IconButton;
+let Colors = mui.Styles.Colors;
+//let Colors = require('../public/js/colors.js');
+let ActionHome = require('../public/js/svg-icons/action-home.jsx');
+let {Link, RouteHandler} = require('react-router');
 
 const AppBar = React.createClass({
   render() {
+    let appBarTitle = '上海大学';
     return (
-      <AppBarComponent title="上海大学" />
+      <AppBarComponent
+        showMenuIconButton={true}
+        iconElementLeft={<Link to="/">
+          <IconButton tooltip="Home" touch={true}>
+            <ActionHome color={Colors.white} hoverColor={Colors.cyan900} />
+          </IconButton>
+        </Link>}
+        title={appBarTitle} />
     )
   }
 })
