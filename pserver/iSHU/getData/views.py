d513a78c82aeed5482bec69b5aa0a37bf571abb2
mio <mio@ty.local>
2015-11-24 18:20:49 +0800

    500

--- pserver/iSHU/getData/views.py
+++ pserver/iSHU/getData/views.py
@@ -6,7 +6,7 @@ from django.http import HttpResponse
 import requests
 
 def index(request):
-
+    print 'aa'
     #t = loader.get_template("index.html") 
     #return HttpResponse(t.render(request))
     return render(request, "index.html")
@@ -37,8 +37,28 @@ def userlogin(request):
                 'password':user_password,
                 }
         #get_info(base_url,append_url,data)
-        login_status = requests.post(base_url+append_url, data = params)
+        login_status = requests.post(base_url+append_url, data = data)
         return login_status
+
+def getcampuscessagelist(request):
+    if request.method == 'GET':
+        import time
+        current_page = request.GET['current_page']
+        base_url = 'http://api.shu.edu.cn/Mobile/'
+        append_url = 'CampusMessage/GetCampusMessageList/'
+        starttime = '2010-01-01T00:00:00Z'
+        endtime = time.strftime('%Y-%m-%dT%H:%M:%SZ')
+        data = { 
+            'keyword':'',
+            'type':203,
+            'limit':10,
+            'startTime':starttime,
+            'endTime':endtime,
+            'currentPage':currentPage,
+        }
+        message_list = requests.post(base_url+append_url, data = data)
+        return message_list.content
+
 
 def campuscalendarspring(request):
     c = requests.get("http://api.shu.edu.cn/Mobile/CampusFile/CampusCalendarSpring")
