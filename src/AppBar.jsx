eeaaeab28f84caeed609f1f7dc1b4a6cb3bbfd72
iVanlIsh <ivan_luoyf@163.com>
2015-11-30 01:31:25 +0800

    appbar finished

--- src/AppBar.jsx
+++ src/AppBar.jsx
@@ -22,97 +22,99 @@ const AppBar = React.createClass({
   getInitialState: function(){
     return { 
       AppBarTitle: '上海大学',
-      // isJoined: false,
-      // showDialogActions: false,
-      // autoHideDuration: 5000,
-      // id: '13121312',
-      // pwd: 'J123123dandi',
-      // status: '请稍等，信息正在空中飞翔。。。',
-      // realname: ''
+      showDialogActions: false,
+      autoHideDuration: 5000,
+      id: '13121312',
+      pwd: 'J123123dandi',
+      status: '请稍等，信息正在空中飞翔。。。',
+      realname: '',
+      logStatus: cookie.load('username')?"登出":"登录"
     }
   },
   componentDidMount: function(){
     this.setState({AppBarTitle: this.props.title});
   },
-  // _handleLogin: function(){
-  //   // this.setState({status: '请稍等，信息正在空中飞翔。。。'});
-  //   this.setState({showDialogActions: false});
-  //   this.refs.success.show();
-  //   var data={
-  //     'id': this.state.id,
-  //     'pwd': this.state.pwd,
-  //   };
-  //   console.log(data);
-  //   $.ajax({
-  //     url: 'userlogin',
-  //     dataType: 'json',
-  //     method: 'post',
-  //     data: data,
-  //     success: function(data) {
-  //       var t_status = data.status;
-  //       this.setState({status: t_status,message: t_status});
-  //       if (t_status == "登录成功") {
-  //         var t_realname = data.realname;
-  //         this.setState({realname: t_realname});
-  //         cookie.save('username', data.username);
-  //       };
-  //     }.bind(this),
-  //     error: function(xhr, status, err) {
-  //       this.setState({status: '登陆失败'});
-  //       console.error(this.props.url, status, err.toString());
-  //     }.bind(this)
-  //   });
-  // },
-  // _handleRequestClose: function(){
-  //   this.setState({showDialogActions: false});
-  // },
-  // _handleDialogCancel: function(){
-  //   this.setState({showDialogActions: false});
-  // },
-  // _handleAction: function(event){
-  //   this.refs.success.dismiss();
-  // },
-  // idHandleChange: function(event) {
-  //   this.setState({id: event.target.value});
-  // },
-  // pwdHandleChange: function(event) {
-  //   this.setState({pwd: event.target.value});
-  // },
-  _handleLoginLogout: function(event, item){
-    console.log(item);
-    if(item == "登出") {
-      // cookie.remove('username');
+  _handleLogin: function(){
+    // this.setState({status: '请稍等，信息正在空中飞翔。。。'});
+    this.setState({showDialogActions: false});
+    this.refs.success.show();
+    var data={
+      'id': this.state.id,
+      'pwd': this.state.pwd,
+    };
+    console.log(data);
+    $.ajax({
+      url: 'userlogin',
+      dataType: 'json',
+      method: 'post',
+      data: data,
+      success: function(data) {
+        var t_status = data.status;
+        this.setState({status: t_status,message: t_status});
+        if (t_status == "登录成功") {
+          var t_realname = data.realname;
+          this.setState({realname: t_realname});
+          cookie.save('username', data.username);
+          this.setState({logStatus: "登出"});
+        };
+      }.bind(this),
+      error: function(xhr, status, err) {
+        this.setState({status: '登陆失败'});
+        console.error(this.props.url, status, err.toString());
+      }.bind(this)
+    });
+  },
+  _handleRequestClose: function(){
+    this.setState({showDialogActions: false});
+  },
+  _handleDialogCancel: function(){
+    this.setState({showDialogActions: false});
+  },
+  _handleAction: function(event){
+    this.refs.success.dismiss();
+  },
+  idHandleChange: function(event) {
+    this.setState({id: event.target.value});
+  },
+  pwdHandleChange: function(event) {
+    this.setState({pwd: event.target.value});
+  },
+  _handleLoginLogout: function(){
+    if(this.state.logStatus == "登出") {
+      cookie.remove('username');
+      this.setState({logStatus: "登录"});
       console.log("123");
     }
-    if(item == "登录"){
-      // this.setState({showDialogActions: true});
+    if(this.state.logStatus == "登录"){
+      this.setState({showDialogActions: true});
       console.log("321");
     }  
   },
-  render() {
-    let logStatus = cookie.load() ? "登出" : "登录";
-    // let customActions = [
-    //   <FlatButton
-    //     label="取消"
-    //     secondary={true}
-    //     onTouchTap={this._handleDialogCancel} />,
-    //   <FlatButton
-    //     label="登陆"
-    //     primary={true}
-    //     onTouchTap={this._handleLogin} />
-    // ];
-    // let styles = {
-    //   content : {
-    //     width: '100%',
-    //     position: 'relative',
-    //     zIndex: 10,
-    //   },
-    //   main : {
-    //     position: 'fixed'
-    //     // position: 'absolute'
-    //   }
-    // };
+  render: function() {
+    let logstatus = this.state.logStatus;
+    let customActions = [
+      <FlatButton
+        label="取消"
+        secondary={true}
+        onTouchTap={this._handleDialogCancel} />,
+      <FlatButton
+        label="登录"
+        primary={true}
+        onTouchTap={this._handleLogin} />
+    ];
+    let styles = {
+      content : {
+        width: '100%',
+        position: 'relative',
+        zIndex: 10,
+      },
+      main : {
+        position: 'fixed'
+        // position: 'absolute'
+      }
+    };
     return (
+      <div>
         <AppBarComponent
           title={this.state.AppBarTitle}
           showMenuIconButton={true}
@@ -124,9 +126,35 @@ const AppBar = React.createClass({
             </Link>}
           iconElementRight={
             <IconMenu iconButtonElement={ <IconButton><MoreVertIcon /></IconButton> }>
-              <MenuItem primaryText={logStatus}
-                onChange={this._handleLoginLogout(event, logStatus)} />
+              <MenuItem primaryText={logstatus}
+                onTouchTap={this._handleLoginLogout} />
             </IconMenu>} />
+            <Dialog
+            ref="login"
+            title="登录"
+            actions={customActions}
+            open={this.state.showDialogActions}
+            autoDetectWindowHeight={true}
+            autoScrollBodyContent={true}
+            onRequestClose={this._handleRequestClose}
+            contentStyle={styles.content}
+            style={styles.main}>
+            <div className="index">
+        <TextField className="text-field"
+          floatingLabelText="学  号" type='id' onChange={this.idHandleChange}/>
+        <br></br>
+        <TextField className="text-field"
+          floatingLabelText="密  码"
+          type = "password" onChange={this.pwdHandleChange}/>
+      </div>
+          </Dialog>
+          <Snackbar
+            ref="success"
+            message={this.state.status}
+            action="关闭"
+            autoHideDuration={this.state.autoHideDuration}
+            onActionTouchTap={this._handleAction}/>
+            </div>
     )
   }
 });
