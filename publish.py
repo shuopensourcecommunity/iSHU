3a8f0ceaff922b82f8946cbc833248f07889f88a
SimonZheng <bucketzxm@gmail.com>
2015-12-15 23:42:54 +0800

    add gitignore; add publish-fab file to publish

---
+++ publish.py
@@ -0,0 +1,31 @@
+#!/bin/env python
+from __future__ import print_function
+from fabric.api import *
+from string import strip
+
+WORK_DIR = '/var/www/ishu'
+def read_config():
+    with open('server.config', 'r') as f:
+        env.host_string, env.user, env.password = map(strip, f.readlines())
+        print(env.host_string, env.user, env.password)
+
+
+def push():
+    ''' push local code to server
+
+    '''
+    read_config()
+    local('npm install')
+    local('webpack --config webpack.config.js')
+    run('mkdir -p {}'.format(WORK_DIR))
+    with cd('/var/www/ishu'):
+        run('mkdir -p build public style pserver')
+        put('build/*','./build')
+        put('style/*', './style')
+        put('public/*','./public')
+        put('pserver/*','./pserver')
+        put('./index.html','./index.html')
+
+
+if __name__ == "__main__":
+    read_config()
