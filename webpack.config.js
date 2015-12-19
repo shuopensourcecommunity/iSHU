74f95bf3a05b3226b63cee4b594f3cf9679bf6ac
iVanlIsh <ivan_luoyf@163.com>
2015-12-19 17:47:07 +0800

    index.html

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports ={
     },
     output: {
         path: path.join(__dirname, 'build'),
-        filename: "[name]bundle.js"
+        filename: "[name]_bundle.js"
     },
     module: {
         loaders: [
