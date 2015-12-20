5d8141e1d3f9b6125beb71989335e38230f2bc53
Simon Zheng <bucketzxm@gmail.com>
2015-12-20 20:41:33 +0800

    reformat code style

--- pserver/iSHU/askbar/views.py
+++ pserver/iSHU/askbar/views.py
@@ -13,15 +13,14 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECTIONS = ['Categories', 'AskList', 'Question']
 
 urls = {
-	'Categories':'http://api.shu.edu.cn/Mobile/Lehu/Categories',
-	'AskList':	 'http://api.shu.edu.cn/Mobile/Lehu/AskList',
-	'Qusetion':	 'http://api.shu.edu.cn/Mobile/Lehu/Question',
+    'Categories': 'http://api.shu.edu.cn/Mobile/Lehu/Categories',
+    'AskList': 'http://api.shu.edu.cn/Mobile/Lehu/AskList',
+    'Qusetion': 'http://api.shu.edu.cn/Mobile/Lehu/Question',
 }
 
 
 @require_http_methods(["GET"])
 def get_info(request, section):
-
     def generate_view():
         with open(os.path.join(BASE_DIR, 'askbar/section.json'), "r") as f:
             sections = json.load(f)
@@ -33,23 +32,23 @@ def get_info(request, section):
 
     data = generate_view()
     if data.has_key('questionId'):
-    	date['questionId'] = request.GET.get('questionId')
+        date['questionId'] = request.GET.get('questionId')
     if data.has_key('cid'):
-    	date['cid'] = request.GET.get('cid')
+        date['cid'] = request.GET.get('cid')
     if data.has_key('page'):
-    	date['page'] = request.GET.get('page')
+        date['page'] = request.GET.get('page')
 
-	msg_res = requests.get(urls[section],data = data).json()
-	if msg_res.has_key('Data'):
-	    msg_list = msg_res['Data']
+        msg_res = requests.get(urls[section], data=data).json()
+        if msg_res.has_key('Data'):
+            msg_list = msg_res['Data']
 
-	response = dict()
-	for i in range(0, len(msg_list)):
-	    c = dict()
-	    for key, value in msg_list[i].iteritems():
-	        if isinstance(value, (unicode,)):
-	            c[key] = value
-	        else:
-	            c[key] = unicode(value)
-	    response[unicode(i+1)] = c
-	return JsonResponse(response)
+        response = dict()
+        for i in range(0, len(msg_list)):
+            c = dict()
+            for key, value in msg_list[i].iteritems():
+                if isinstance(value, (unicode,)):
+                    c[key] = value
+                else:
+                    c[key] = unicode(value)
+            response[unicode(i + 1)] = c
+        return JsonResponse(response)
