5a5d60976e0857ce503f1293bbd316ddfeb3dfce
ywtesi <jyuwei0123@gmail.com>
2015-12-27 20:26:22 +0800

    [react-router] fix {Home} route

--- src/shu_ask/main.jsx
+++ src/shu_ask/main.jsx
@@ -14,7 +14,6 @@ const {Router, Route, Link, History, Lifecycle} = require('react-router');
 render((
   <Router>
     <Route path="/" component={Home} />
-    <Route path="/:key" component={Home}/>
     <Route path="login" component={Login} />
     <Route path="about" component={About} />
     <Route path="question" component={QuestionAnswer.Question} />
