6dd7c470bbcabe403c4b1040e59250831a41aa53
Simon Zheng <bucketzxm@gmail.com>
2015-12-28 19:47:19 +0800

    add space choose to strip function

--- pserver/iSHU/askbar/utils.py
+++ pserver/iSHU/askbar/utils.py
@@ -2,16 +2,17 @@
 import re
 
 
-def strip_tags(raw):
+def strip_tags(raw, space=False):
     """
 
     Args:
         raw: raw data with html tags and &nbsp symbol
-
+        space: choose ignore space or not
     Returns:
         :cooked clean content data
     """
     pattern = r'</?\w+[^>]*>'
     cooked = re.sub(pattern, '', raw)
-    cooked = re.sub('&nbsp', cooked)
+    if space:
+        cooked = re.sub('&nbsp', cooked)
     return cooked
