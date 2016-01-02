503a6172dabe45a667e2e1b7db4ea303187908cf
Simon Zheng <bucketzxm@gmail.com>
2016-01-03 00:06:05 +0800

    stupid typo error

--- pserver/iSHU/askbar/urls.py
+++ pserver/iSHU/askbar/urls.py
@@ -11,7 +11,7 @@ urlpatterns = [
     url(r'^getAskList', get_ask_list, name='get_ask_list'),
     url(r'^getAnswerByQuestionId', get_answer_by_question_id, name='get_answer_by_questionId'),
     url(r'^getAnswerDetailById', get_answer_detail_by_id, name='get_answer_detail_by_id'),
-    url('r^getQuestionDetailById', get_question_detail_by_id, name='get_question_detail_by_id'),
-    url('r^submitQuestion', submit_question, name='submit_question'),
-    url('r^submitAnswer', submit_answer, name='submit_answer')
+    url(r'^getQuestionDetailById', get_question_detail_by_id, name='get_question_detail_by_id'),
+    url(r'^submitQuestion', submit_question, name='submit_question'),
+    url(r'^submitAnswer', submit_answer, name='submit_answer')
 ]
