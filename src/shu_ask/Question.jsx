82a96972a07c24b2fe53052a2cdd81f62006f828
ywtesi <jyuwei0123@gmail.com>
2016-01-06 23:15:38 +0800

    update select-category-when-ask

--- src/shu_ask/Question.jsx
+++ src/shu_ask/Question.jsx
@@ -4,30 +4,50 @@ const HeadBar = require('./HeadBar.jsx');
 const React = require('react');
 const cookie = require('react-cookie');
 const {Link, RouteHandler} = require('react-router');
-const {FlatButton, SelectField, TextField, menuItems} = require('material-ui');
+const {FlatButton, SelectField, TextField, menuItems, MenuItem} = require('material-ui');
 
 const Question = React.createClass({
   getInitialState: function() {
     return {
-      values: [
-        { payload: '1', text: '新生入学' },
-        { payload: '2', text: '招生情况' },
-        { payload: '3', text: '学习制度' },
-        { payload: '4', text: '学生组织' }
-      ],
+      categories: [],
     	title: '',
     	content: '',
     	cid: 1
     };
   },
-  _handleSelectValueChange: function(values){
-  	console.log(values.target.value);
-    this.setState({cid: values.target.value});
+  loadCategoriesFromServer: function() {
+    $.ajax({
+      url: 'categories',
+      dataType: 'json',
+      methods: 'get',
+      success: function(data) {
+				// console.log(data);
+        let t_categories = [];
+        for (let obj in data.Data){
+          t_categories.push({
+            'id': data.Data[obj].ID,
+            'name': data.Data[obj].Name
+          });
+        }
+        this.setState({
+          categories: t_categories
+        });
+      }.bind(this),
+      error: function(xhr, status, err) {
+        console.error(this.props.url, status, err.toString());
+      }.bind(this)
+    });
   },
-  _handleTitleChange: function(event){
+  componentDidMount: function() {
+    this.loadCategoriesFromServer();
+  },
+  handleSelectValueChange: function(e, index, value){
+    this.setState({cid: value});
+  },
+  handleTitleChange: function(event){
     this.setState({title: event.target.value});
   },
-  _handleContentChange: function(event){
+  handleContentChange: function(event){
     this.setState({content: event.target.value});
   },
   handleSubmitQuestion: function() {
@@ -45,14 +65,6 @@ const Question = React.createClass({
       data: data,
       success: function(data) {
         console.log(data);
-        // var t_status = data.status;
-        // this.setState({status: t_status,message: t_status});
-        // if (t_status == "登录成功") {
-        //   var t_realname = data.realname;
-        //   this.setState({realname: t_realname});
-        //   cookie.save('username', data.username);
-        //   this.setState({logStatus: "登出"});
-        // };
       }.bind(this),
       error: function(xhr, status, err) {
         console.error(this.props.url, status, err.toString());
@@ -60,6 +72,7 @@ const Question = React.createClass({
     });
   },
   render: function() {
+    // console.log(this.state.cid);
     var style = {
       textfield: {
       	textAlign: 'left'
@@ -81,17 +94,20 @@ const Question = React.createClass({
        			labelStyle={style.label}
 	        	value={this.state.cid}
 	        	floatingLabelText='问题分类'
-  	     		valueMember='payload'
-	    	   	displayMember='text'
-  	   	  	menuItems={this.state.values}
-    	   		onChange={this._handleSelectValueChange.bind('values')} />
+    	   		onChange={this.handleSelectValueChange} >
+            {
+              this.state.categories.map(category =>
+                <MenuItem value={category.id} primaryText={category.name} />
+              )
+            }
+          </SelectField>
     ];
     return (
       <div>
         <HeadBar title='提问' />
         <center>
         	<TextField
-        		onChange={this._handleTitleChange}
+        		onChange={this.handleTitleChange}
         		floatingLabelText='提问标题' />
         </center>
         <center>
@@ -99,7 +115,7 @@ const Question = React.createClass({
         		style={style.textfield}
           	multiLine={true}
           	rows={8}
-        		onChange={this._handleContentChange}
+        		onChange={this.handleContentChange}
           	floatingLabelText='问题描述' />
         </center>
         {field}
